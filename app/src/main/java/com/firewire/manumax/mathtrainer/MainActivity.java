48445e8817fa478dba062df31cf43e2332c18249
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-08-20 21:19:13 +0530

    project began

---
+++ app/src/main/java/com/firewire/manumax/mathtrainer/MainActivity.java
@@ -0,0 +1,23 @@
+package com.firewire.manumax.mathtrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGo;
+
+    public void start(View view){
+        btnGo.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGo=(Button)findViewById(R.id.btnGo);
+    }
+}
