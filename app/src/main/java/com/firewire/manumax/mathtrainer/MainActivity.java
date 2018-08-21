52dad15fa7c3f262093bb6ee8b20cf5b4823b998
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-08-21 21:08:17 +0530

    error occured

--- app/src/main/java/com/firewire/manumax/mathtrainer/MainActivity.java
+++ app/src/main/java/com/firewire/manumax/mathtrainer/MainActivity.java
@@ -13,41 +13,10 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnGo;
-    ArrayList<Integer> answers;
-    int locationOfCorrectAnswer;
-
-    public void start(View view){
-        btnGo.setVisibility(View.INVISIBLE);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnGo=(Button)findViewById(R.id.btnGo);
-        TextView sumTextView=(TextView)findViewById(R.id.sumTextView);
-
-        Random rand=new Random();
-        int a=rand.nextInt(16);
-        int b=rand.nextInt(16);
-
-        sumTextView.setText(Integer.toString(a)+" + "+Integer.toString(b));
-        locationOfCorrectAnswer=rand.nextInt(4);
-
-        for(int i=0;i<4;i++){
-            if(i==locationOfCorrectAnswer){
-                answers.add(a+b);
-            }else{
-                int incorrectAnswer=rand.nextInt(31);
-
-                //now we need while loop for check is incorrect answer equal to correct answer if true we generate another random number
-                while(incorrectAnswer==a+b){
-                    incorrectAnswer=rand.nextInt(31);
-                }
-                
-            }
-        }
     }
 }
